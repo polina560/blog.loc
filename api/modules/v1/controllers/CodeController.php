33b77a8118fc05d421e7aaec2471187b2a38fca7
polina560 <bobrrrrovapolina@gmail.com>
2024-11-21 18:10:53 +0300

    2 задача для API

--- api/modules/v1/controllers/CodeController.php
+++ api/modules/v1/controllers/CodeController.php
@@ -38,14 +38,19 @@ class CodeController extends AppController
     public function actionIndex(): array
     {
         $code = $this->getParameterFromRequest('code');
+        // валидация
         $user_id = Yii::$app->user->identity->getId();
-
+        $user = UserExt::find()->where(['user_id' => $user_id])->one();
+        // Не забанен ли уже
+        $currentTime = time();
+//        if ($user->banned_at > time() - $params['period']) {
+//
+//        }
+        return $this->returnError('Подождите N времени');
         $codeModel = Code::find()->where(['code' => $code])->one();
 
         if (empty($code) || empty($codeModel)) {
-            $user = UserExt::find()->where(['user_id' => $user_id])->one();
-            $invalid_code_cnt = $user->invalid_code + 1;
-            $user->load(['user_id' => $invalid_code_cnt], '');
+            $user->invalid_code += 1;
             $user->save();
             return $this->returnError(['Поле code не заполнено или заполнено некорректно']);
         }
@@ -65,4 +70,10 @@ class CodeController extends AppController
 
 
     }
+
+    //фун-ии 1) проверить забаненность, забанить, снять бан
+//     ArrayHelper::getValue(Yii::$app->params, 'request_limits.' . $category);
+
+
+
 }
